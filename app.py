cc74d36e8d750522d5462095e38b24a8dbf71b07
admin <elliotarcher17@hotmail.co.uk>
2024-08-19 17:46:23 +0100

    update pooper

---
+++ app.py
@@ -0,0 +1,116 @@
+import sqs_listener
+import boto3
+import json
+import logging
+import requests
+from functions import trigger_scrape_match,trigger_match_lvl_summary, trigger_leg_lvl_summary, trigger_dpl_price, trigger_match_length, telegram_bot_sendtext
+
+access_key_id = 'AKIA467ST2DY3IRHUUWY'
+secret_access_key = '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'
+
+# Configure logging
+logging.basicConfig(filename='betfair_stream.log', level=logging.INFO,
+                    format='%(asctime)s:%(levelname)s:%(message)s')
+
+# Create SQS client
+sqs = boto3.client('sqs', region_name='eu-west-1',
+                   aws_access_key_id=access_key_id,
+                   aws_secret_access_key=secret_access_key)
+
+scrape_failed_dict = {}
+
+class MyListener(sqs_listener.SqsListener):
+    def handle_message(self, body, attributes, message_attributes):
+        topic = body['Topic']
+        json_str = body['Body']
+        if topic == 'New Event':
+            fixture_id = json_str.get('fixture_id')
+            openDate = json_str.get('openDate')
+            print(f'{fixture_id}: New Event')
+            a_name = json_str.get('a_name')
+            b_name = json_str.get('b_name')
+            competition = json_str.get('competition')
+            eventId = json_str.get('eventId')
+            a_id = json_str.get('a_id')
+            b_id = json_str.get('b_id')
+            trigger_dpl_price(fixture_id=fixture_id,
+                              openDate=openDate,
+                              eventId=eventId,
+                              competition=competition,
+                              a_name=a_name,
+                              b_name=b_name,
+                              a_id=a_id,
+                              b_id=b_id
+                              )
+            logging.info(f"Sent match price request")
+
+        if topic == 'New Market':
+            print('New Market')
+
+        if topic == 'Market Finished':
+            print('Market Finished')
+
+        if topic == 'Event Finished':
+            fixture_id = json_str.get('fixture_id')
+            print(f'{fixture_id}: Event Finished')
+            a_name = json_str.get('a_name')
+            b_name = json_str.get('b_name')
+            trigger_scrape_match(a_name, b_name, fixture_id)
+
+        if topic == 'Scrape Failed':
+            fixture_id = json_str.get('fixture_id')
+            if fixture_id in scrape_failed_dict:
+                scrape_failed_dict[fixture_id] += 1
+            else:
+                scrape_failed_dict[fixture_id] = 1
+            selection_a = json_str.get('selection_a')
+            print(f'{fixture_id}: Scrape Failed')
+            trigger_scrape_match(selection_a, 'Unknown', fixture_id)
+
+        if topic == 'Match Scraped':
+            print(f'Match Scraped')
+            trigger_match_lvl_summary(body)
+            trigger_leg_lvl_summary(body)
+            # match scraped
+            # trigger match_lvl_summary
+
+        if topic == 'Match Summary Success':
+            fixture_id = json_str.get('fixture_id')
+            if fixture_id not in scraping_dict:
+                scraping_dict[fixture_id] = scraping_dict_child
+                scraping_dict[fixture_id]['Match'] = 1
+            else:
+                scraping_dict[fixture_id]['Match'] = 1
+            if scraping_dict[fixture_id]['Legs'] == 1 and scraping_dict[fixture_id]['Match'] == 1:
+                telegram_bot_sendtext(f'{fixture_id}: Full Scrape Complete')
+
+        if topic == 'Leg Summary Success':
+            fixture_id = json_str.get('fixture_id')
+            if fixture_id not in scraping_dict:
+                scraping_dict[fixture_id] = scraping_dict_child
+                scraping_dict[fixture_id]['Legs'] = 1
+            else:
+                scraping_dict[fixture_id]['Legs'] = 1
+            if scraping_dict[fixture_id]['Legs'] == 1 and scraping_dict[fixture_id]['Match'] == 1:
+                telegram_bot_sendtext(f'{fixture_id}: Full Scrape Complete')
+
+        if topic == 'Price Requested':
+            tournament = json_str.get('tournament')
+            fixture_id = json_str.get('fixture_id')
+            trigger_dpl_price(fixture_id, tournament)
+
+        if topic == 'Length Requested':
+            fixture_id = json_str.get('fixture_id')
+            tournament = json_str.get('tournament')
+            print(f'length: {fixture_id}, {tournament}')
+            trigger_match_length(fixture_id, tournament)
+
+scraping_dict = {}
+scraping_dict_child = {'Match': 0, 'Legs': 0}
+
+listener = MyListener('Pooper',
+                      region_name='eu-west-1',
+                      aws_access_key='AKIA467ST2DY3IRHUUWY',
+                      aws_secret_key='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')
+
+listener.listen()
---
+++ functions.py
@@ -0,0 +1,164 @@
+import requests
+import boto3
+from botocore.exceptions import ClientError
+import json
+
+def telegram_bot_sendtext(text):
+    bot_token = "5862237285:AAEgN1I94fgt8oT39LFNT__-sRfBR9liX0I"
+    bot_chatid = '5897205121'
+    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' \
+                + bot_chatid + '&parse_mode=Markdown&text=' + text
+
+    response = requests.get(send_text).json()
+
+    return response
+
+
+def trigger_fixture_query():
+    import boto3
+    import json
+    from botocore.config import Config
+
+    config = Config(retries={'max_attempts': 0})
+    lambda_client = boto3.client('lambda',
+                                 region_name='eu-west-1',
+                                 aws_access_key_id='AKIA467ST2DY3IRHUUWY',
+                                 aws_secret_access_key='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',
+                                 config=config
+                                 )
+    lambda_payload = {"Event": "Start"}
+    lambda_payload = json.dumps(lambda_payload)
+    lambda_client.invoke(FunctionName='fixture_query',
+                         InvocationType='Event',
+                         Payload=lambda_payload)
+
+
+def trigger_sim_full_match(fixtureid):
+    import boto3
+    import json
+    from botocore.config import Config
+
+    config = Config(retries={'max_attempts': 0})
+    lambda_client = boto3.client('lambda',
+                                 region_name='eu-west-1',
+                                 aws_access_key_id='AKIA467ST2DY3IRHUUWY',
+                                 aws_secret_access_key='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',
+                                 config=config,
+                                 )
+    lambda_payload = {"fixtureid": f"{fixtureid}"}
+    lambda_payload = json.dumps(lambda_payload)
+    lambda_client.invoke(FunctionName='sim_full_match',
+                         InvocationType='Event',
+                         Payload=lambda_payload,
+                         )
+
+
+def trigger_dpl_price(fixture_id, openDate, eventId, competition, a_name, b_name, a_id, b_id):
+    import boto3
+    import json
+    from botocore.config import Config
+
+    config = Config(retries={'max_attempts': 0})
+    lambda_client = boto3.client('lambda',
+                                 region_name='eu-west-1',
+                                 aws_access_key_id='AKIA467ST2DY3IRHUUWY',
+                                 aws_secret_access_key='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',
+                                 config=config,
+                                 )
+    lambda_payload = {"fixtureid": fixture_id,
+                      "openDate": openDate,
+                      "eventId": eventId,
+                      "competition": competition,
+                      "selection_a": a_name,
+                      "selection_b": b_name,
+                      "a_id": a_id,
+                      "b_id": b_id
+                      }
+    lambda_payload = json.dumps(lambda_payload)
+    lambda_client.invoke(FunctionName='price_match',
+                         InvocationType='Event',
+                         Payload=lambda_payload,
+                         )
+
+
+def trigger_scrape_match(selection_a, selection_b, fixture_id):
+    try:
+        # Initialize the Lambda client
+        lambda_client = boto3.client('lambda',
+                                     region_name='eu-west-1',
+                                     aws_access_key_id='AKIA467ST2DY3IRHUUWY',
+                                     aws_secret_access_key='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')
+
+        # Construct the payload
+        lambda_payload = {"selection_a": selection_a, "selection_b": selection_b, "fixture_id": fixture_id}
+
+        # Serialize the JSON payload
+        lambda_payload_str = json.dumps(lambda_payload)
+
+        # Invoke the Lambda function
+        response = lambda_client.invoke(FunctionName='lambda_scrape_match',
+                                        InvocationType='Event',
+                                        Payload=lambda_payload_str)
+
+    except ClientError as e:
+        print(f"Error invoking Lambda function: {e.response['Error']['Message']}")
+
+
+def trigger_match_lvl_summary(json_payload):
+    try:
+        # Initialize the Lambda client
+        lambda_client = boto3.client('lambda',
+                                     region_name='eu-west-1',
+                                     aws_access_key_id='AKIA467ST2DY3IRHUUWY',
+                                     aws_secret_access_key='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')
+
+        # Serialize the JSON payload
+        lambda_payload = json.dumps(json_payload)
+
+        # Invoke the Lambda function
+        response = lambda_client.invoke(FunctionName='match_lvl_summary',
+                                        InvocationType='Event',
+                                        Payload=lambda_payload)
+
+    except ClientError as e:
+        print(f"Error invoking Lambda function: {e.response['Error']['Message']}")
+
+
+def trigger_leg_lvl_summary(json_payload):
+    try:
+        # Initialize the Lambda client
+        lambda_client = boto3.client('lambda',
+                                     region_name='eu-west-1',
+                                     aws_access_key_id='AKIA467ST2DY3IRHUUWY',
+                                     aws_secret_access_key='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')
+
+        # Serialize the JSON payload
+        lambda_payload = json.dumps(json_payload)
+
+        # Invoke the Lambda function
+        response = lambda_client.invoke(FunctionName='leg_lvl_summary',
+                                        InvocationType='Event',
+                                        Payload=lambda_payload)
+
+    except ClientError as e:
+        print(f"Error invoking Lambda function: {e.response['Error']['Message']}")
+
+
+def trigger_match_length(fixtureid, tournament):
+    import boto3
+    import json
+    from botocore.config import Config
+
+    config = Config(retries={'max_attempts': 0})
+    lambda_client = boto3.client('lambda',
+                                 region_name='eu-west-1',
+                                 aws_access_key_id='AKIA467ST2DY3IRHUUWY',
+                                 aws_secret_access_key='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',
+                                 config=config,
+                                 )
+    lambda_payload = {"fixture_id": f"{fixtureid}", "tournament": tournament}
+    lambda_payload = json.dumps(lambda_payload)
+    lambda_client.invoke(FunctionName='sportradar_get_match_length',
+                         InvocationType='Event',
+                         Payload=lambda_payload,
+                         )
