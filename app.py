b046a361be6a168c74ca8ee25b64542aebdfd31b
admin <elliotarcher17@hotmail.co.uk>
2024-08-21 21:24:29 +0100

    add logging

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ secret_access_key = 'dChJGYE2L1TksjJI73RSB9Iire4P9FzQsgKPXNQx'
 # Configure logging
 logging.basicConfig(filename='betfair_stream.log', level=logging.INFO,
                     format='%(asctime)s:%(levelname)s:%(message)s')
+
 logging.info("Starting App")
 
 # Create SQS client
@@ -26,25 +27,28 @@ class MyListener(sqs_listener.SqsListener):
         topic = body['Topic']
         json_str = body['Body']
         if topic == 'New Event':
-            fixture_id = json_str.get('fixture_id')
-            openDate = json_str.get('openDate')
-            a_name = json_str.get('a_name')
-            b_name = json_str.get('b_name')
-            competition = json_str.get('competition')
-            eventId = json_str.get('eventId')
-            logging.info(f"Received New Event {fixture_id}")
-            a_id = json_str.get('a_id')
-            b_id = json_str.get('b_id')
-            trigger_dpl_price(fixture_id=fixture_id,
-                              openDate=openDate,
-                              eventId=eventId,
-                              competition=competition,
-                              a_name=a_name,
-                              b_name=b_name,
-                              a_id=a_id,
-                              b_id=b_id
-                              )
-            logging.info(f"Sent match price request")
+            try:
+                fixture_id = json_str.get('fixture_id')
+                openDate = json_str.get('openDate')
+                a_name = json_str.get('a_name')
+                b_name = json_str.get('b_name')
+                competition = json_str.get('competition')
+                eventId = json_str.get('eventId')
+                logging.info(f"Received New Event {fixture_id}")
+                a_id = json_str.get('a_id')
+                b_id = json_str.get('b_id')
+                trigger_dpl_price(fixture_id=fixture_id,
+                                  openDate=openDate,
+                                  eventId=eventId,
+                                  competition=competition,
+                                  a_name=a_name,
+                                  b_name=b_name,
+                                  a_id=a_id,
+                                  b_id=b_id
+                                  )
+                logging.info(f"Sent match price request")
+            except:
+                logging.error(f"Error sending New Event price request")
 
         if topic == 'New Market':
             logging.info(f"Received New Market")
@@ -53,66 +57,81 @@ class MyListener(sqs_listener.SqsListener):
             logging.info(f"Received Market Finished")
 
         if topic == 'Event Finished':
-            fixture_id = json_str.get('fixture_id')
-            logging.info(f"Received New Event {fixture_id}")
-            a_name = json_str.get('a_name')
-            b_name = json_str.get('b_name')
-            trigger_scrape_match(a_name, b_name, fixture_id)
+            try:
+                fixture_id = json_str.get('fixture_id')
+                logging.info(f"Received New Event {fixture_id}")
+                a_name = json_str.get('a_name')
+                b_name = json_str.get('b_name')
+                trigger_scrape_match(a_name, b_name, fixture_id)
+            except:
+                logging.error(f"Error sending Event Finished scrape")
 
         if topic == 'Scrape Failed':
-            fixture_id = json_str.get('fixture_id')
-            if fixture_id in scrape_failed_dict:
-                scrape_failed_dict[fixture_id] += 1
-            else:
-                scrape_failed_dict[fixture_id] = 1
-            selection_a = json_str.get('selection_a')
-            logging.info(f"Received Scrape Failed {fixture_id}")
-            trigger_scrape_match(selection_a, 'Unknown', fixture_id)
+            try:
+                fixture_id = json_str.get('fixture_id')
+                if fixture_id in scrape_failed_dict:
+                    scrape_failed_dict[fixture_id] += 1
+                else:
+                    scrape_failed_dict[fixture_id] = 1
+                selection_a = json_str.get('selection_a')
+                logging.info(f"Received Scrape Failed {fixture_id}")
+                trigger_scrape_match(selection_a, 'Unknown', fixture_id)
+            except:
+                logging.error(f"Error sending Scrape Failed rescrape")
 
         if topic == 'Match Scraped':
-            logging.info(f"Received Match Scraped")
-            trigger_match_lvl_summary(body)
-            trigger_leg_lvl_summary(body)
-            # match scraped
-            # trigger match_lvl_summary
+            try:
+                logging.info(f"Received Match Scraped")
+                trigger_match_lvl_summary(body)
+                trigger_leg_lvl_summary(body)
+                # match scraped
+                # trigger match_lvl_summary
+            except:
+                logging.error(f"Error sending Match Scraped trigger summary")
 
         if topic == 'Match Summary Success':
-            fixture_id = json_str.get('fixture_id')
-            a_name = json_str.get('a_name')
-            b_name = json_str.get('b_name')
-            logging.info(f"Received Match Summary {fixture_id}")
-            if fixture_id not in scraping_dict:
-                scraping_dict[fixture_id] = scraping_dict_child
-                scraping_dict[fixture_id]['Match'] = 1
-            else:
-                scraping_dict[fixture_id]['Match'] = 1
-            if scraping_dict[fixture_id]['Legs'] == 1 and scraping_dict[fixture_id]['Match'] == 1:
-                telegram_bot_sendtext(f'{fixture_id}: Full Scrape Complete')
-                url = 'http://127.0.0.1:5000//trigger_related_dpl_price'
-                headers = {'Content-Type': 'application/json'}
-                data = {"a_name": a_name,
-                        "b_name": b_name}
-                response = requests.post(url, headers=headers, data=json.dumps(data))
-                logging.info(f"Sent trigger prices {fixture_id}")
+            try:
+                fixture_id = json_str.get('fixture_id')
+                a_name = json_str.get('a_name')
+                b_name = json_str.get('b_name')
+                logging.info(f"Received Match Summary {fixture_id}")
+                if fixture_id not in scraping_dict:
+                    scraping_dict[fixture_id] = scraping_dict_child
+                    scraping_dict[fixture_id]['Match'] = 1
+                else:
+                    scraping_dict[fixture_id]['Match'] = 1
+                if scraping_dict[fixture_id]['Legs'] == 1 and scraping_dict[fixture_id]['Match'] == 1:
+                    telegram_bot_sendtext(f'{fixture_id}: Full Scrape Complete')
+                    url = 'http://127.0.0.1:5000//trigger_related_dpl_price'
+                    headers = {'Content-Type': 'application/json'}
+                    data = {"a_name": a_name,
+                            "b_name": b_name}
+                    response = requests.post(url, headers=headers, data=json.dumps(data))
+                    logging.info(f"Sent trigger prices {fixture_id}")
+            except:
+                logging.error(f"Error in Match Summary")
 
         if topic == 'Leg Summary Success':
-            fixture_id = json_str.get('fixture_id')
-            a_name = json_str.get('a_name')
-            b_name = json_str.get('b_name')
-            logging.info(f"Received Leg Summary {fixture_id}")
-            if fixture_id not in scraping_dict:
-                scraping_dict[fixture_id] = scraping_dict_child
-                scraping_dict[fixture_id]['Legs'] = 1
-            else:
-                scraping_dict[fixture_id]['Legs'] = 1
-            if scraping_dict[fixture_id]['Legs'] == 1 and scraping_dict[fixture_id]['Match'] == 1:
-                telegram_bot_sendtext(f'{fixture_id}: Full Scrape Complete')
-                url = 'http://127.0.0.1:5000//trigger_related_dpl_price'
-                headers = {'Content-Type': 'application/json'}
-                data = {"a_name": a_name,
-                        "b_name": b_name}
-                response = requests.post(url, headers=headers, data=json.dumps(data))
-                logging.info(f"Sent trigger prices {fixture_id}")
+            try:
+                fixture_id = json_str.get('fixture_id')
+                a_name = json_str.get('a_name')
+                b_name = json_str.get('b_name')
+                logging.info(f"Received Leg Summary {fixture_id}")
+                if fixture_id not in scraping_dict:
+                    scraping_dict[fixture_id] = scraping_dict_child
+                    scraping_dict[fixture_id]['Legs'] = 1
+                else:
+                    scraping_dict[fixture_id]['Legs'] = 1
+                if scraping_dict[fixture_id]['Legs'] == 1 and scraping_dict[fixture_id]['Match'] == 1:
+                    telegram_bot_sendtext(f'{fixture_id}: Full Scrape Complete')
+                    url = 'http://127.0.0.1:5000//trigger_related_dpl_price'
+                    headers = {'Content-Type': 'application/json'}
+                    data = {"a_name": a_name,
+                            "b_name": b_name}
+                    response = requests.post(url, headers=headers, data=json.dumps(data))
+                    logging.info(f"Sent trigger prices {fixture_id}")
+            except:
+                logging.error(f"Error in Leg Summary")
 
 
 scraping_dict = {}
